cda3779feabda9841603bb77111c59efbc4a35df
Rajesh G <rajesh@unigps.in>
2018-07-15 13:11:38 +0530

    base version

---
+++ src/test/java/com/example/interns/InternsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.interns;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InternsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
